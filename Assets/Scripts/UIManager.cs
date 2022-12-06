d412e98dfb4495bb49d77b4ddf5d779677937863
Jorge Sosa Almeida <dorwinrin@gmail.com>
2022-12-06 20:10:29 +0100

    feat(menu): Make required player name

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject playerNameInput;
+
     public void SetPlayerName(string playerName)
     {
         PersistenceManager.Instance.playerName = playerName;
@@ -23,6 +27,13 @@ public class UIManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        if (PersistenceManager.Instance.playerName != "")
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            playerNameInput.GetComponent<Image>().color = Color.yellow;
+        }
     }
 }
